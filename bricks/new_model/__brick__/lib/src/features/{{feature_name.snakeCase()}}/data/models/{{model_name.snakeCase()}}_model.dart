60746102ef21ae3831810650ecfe612e24f5c3a7
Naufal <rnaufal32@muslim.com>
2023-03-17 15:28:47 +0700

    fix model equatable

--- bricks/new_model/__brick__/lib/src/features/{{feature_name.snakeCase()}}/data/models/{{model_name.snakeCase()}}_model.dart
+++ bricks/new_model/__brick__/lib/src/features/{{feature_name.snakeCase()}}/data/models/{{model_name.snakeCase()}}_model.dart
@@ -1,17 +1,20 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+{{#use_equatable}}
 import 'package:equatable/equatable.dart';
+{{/use_equatable}}
 
 part '{{model_name.snakeCase()}}_model.freezed.dart';
 part '{{model_name.snakeCase()}}_model.g.dart';
 
 @freezed
-class {{model_name.pascalCase()}}Model with _${{model_name.pascalCase()}}Model implements Equatable {
+class {{model_name.pascalCase()}}Model with _${{model_name.pascalCase()}}Model {{#use_equatable}}implements Equatable {{/use_equatable}}{
   const {{model_name.pascalCase()}}Model._();
   
   const factory {{model_name.pascalCase()}}Model() = _{{model_name.pascalCase()}}Model;
 
   factory {{model_name.pascalCase()}}Model.fromJson(Map<String, dynamic> json) => _${{model_name.pascalCase()}}ModelFromJson(json);
 
+  {{#use_equatable}}
   @override
   // TODO: implement props
   List<Object?> get props => throw UnimplementedError();
@@ -19,4 +22,5 @@ class {{model_name.pascalCase()}}Model with _${{model_name.pascalCase()}}Model i
   @override
   // TODO: implement stringify
   bool? get stringify => throw UnimplementedError();
+  {{/use_equatable}}
 }
