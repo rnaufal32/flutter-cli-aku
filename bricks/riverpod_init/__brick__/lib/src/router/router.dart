1a8293125f7e95dccd31f8832927ed74bd2da56f
aanr <33546066+rnaufal32@users.noreply.github.com>
2023-05-31 09:40:17 +0700

    Update router.dart

--- bricks/riverpod_init/__brick__/lib/src/router/router.dart
+++ bricks/riverpod_init/__brick__/lib/src/router/router.dart
@@ -1,34 +1,38 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:auto_route/empty_router_widgets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:{{app_name.snakeCase()}}/src/features/auth/presentation/splash/page.dart';
 
 part 'router.gr.dart';
 
-@MaterialAutoRouter(
-  replaceInRouteName: 'Page,Route',
-  routes: [
-    AutoRoute(
-      name: 'AuthRouter',
-      page: EmptyRouterPage,
-      children: [
-        // SIGNIN PAGE
-        // AutoRoute(page: SignInPage),
-        // SIGNUP PAGE
-        // AutoRoute(page: SignUpPage),
-      ],
-    ),
-    AutoRoute(
-      name: 'MainRouter',
-      page: EmptyRouterPage,
-      children: [
-        // MAIN PAGE
-        // AutoRoute(page: MainPage, initial: true),
-      ],
-    ),
-    AutoRoute(page: SplashPage),
-  ],
-)
-class AppRouter extends _$AppRouter {}
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  @override
+  RouteType get defaultRouteType =>
+      const RouteType.material(); //.cupertino, .adaptive ..etc
 
-final routerProvider = Provider.autoDispose((ref) => AppRouter());
+  @override
+  final List<AutoRoute> routes = [
+    AutoRoute(page: SplashRoute.page),
+    AutoRoute(page: AuthRouter.page, children: [
+//       AutoRoute(page: SignInRoute.page, path: ''),
+    ]),
+    AutoRoute(
+      page: MainRouter.page,
+      children: [
+        
+      ],
+    ),
+  ];
+}
+
+@RoutePage(name: 'AuthRouter')
+class AuthPage extends AutoRouter {
+  const AuthPage({super.key});
+}
+
+@RoutePage(name: 'MainRouter')
+class MainPage extends AutoRouter {
+  const MainPage({super.key});
+}
+
+final appRouterProvider = Provider((ref) => AppRouter());
+
