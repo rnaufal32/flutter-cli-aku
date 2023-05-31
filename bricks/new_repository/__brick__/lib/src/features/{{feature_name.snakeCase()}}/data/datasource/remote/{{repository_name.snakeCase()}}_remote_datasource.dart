6958abf31ed361b36c1a7b03a90845d4a6f62b43
aanr <33546066+rnaufal32@users.noreply.github.com>
2023-05-31 13:07:32 +0700

    Update {{repository_name.snakeCase()}}_remote_datasource.dart

--- bricks/new_repository/__brick__/lib/src/features/{{feature_name.snakeCase()}}/data/datasource/remote/{{repository_name.snakeCase()}}_remote_datasource.dart
+++ bricks/new_repository/__brick__/lib/src/features/{{feature_name.snakeCase()}}/data/datasource/remote/{{repository_name.snakeCase()}}_remote_datasource.dart
@@ -1,5 +1,24 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+{{#use_retrofit}}
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
 
+import '../../../../../services/http/http_service.dart';
+
+part 'auth_remote_datasource.g.dart';
+
+@RestApi()
+abstract class {{repository_name.pascalCase()}}RemoteDatasource {}
+  factory {{repository_name.pascalCase()}}RemoteDatasource(Dio dio, {String baseUrl}) =
+      _{{repository_name.pascalCase()}}RemoteDatasource;
+}
+
+final {{repository_name.camelCase()}}RemoteDatasourceProvider = Provider.autoDispose(
+    (ref) => {{repository_name.pascalCase()}}RemoteDatasource(ref.watch(httpServiceProvider)));
+
+{{/use_retrofit}}
+{{^use_retrofit}}
 class {{repository_name.pascalCase()}}RemoteDatasource {}
 
 final {{repository_name.camelCase()}}RemoteDatasourceProvider = Provider.autoDispose((ref) => {{repository_name.pascalCase()}}RemoteDatasource());
+{{/use_retrofit}}
