7d11738f40273479ea20ee2d0c71b0b010bbeead
aanr <33546066+rnaufal32@users.noreply.github.com>
2023-06-21 09:45:11 +0700

    Update {{use_case_name.snakeCase()}}.dart

--- bricks/new_usecase/__brick__/lib/src/features/{{feature_name.snakeCase()}}/domain/use_case/{{use_case_name.snakeCase()}}.dart
+++ bricks/new_usecase/__brick__/lib/src/features/{{feature_name.snakeCase()}}/domain/use_case/{{use_case_name.snakeCase()}}.dart
@@ -4,7 +4,7 @@ import '../../data/repositories/{{feature_name.snakeCase()}}_repository.dart';
 import '../repositories/{{feature_name.snakeCase()}}_repository.dart';
 
 class {{use_case_name.pascalCase()}}UseCase {
-  final HolidayRepository repository;
+  final {{feature_name.pascalCase()}}Repository repository;
 
   {{use_case_name.pascalCase()}}UseCase(this.repository);
 
