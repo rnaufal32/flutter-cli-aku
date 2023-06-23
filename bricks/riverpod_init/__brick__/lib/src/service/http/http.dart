f2a6e3c4e09cdfd2206c12a2cfed860e2947e471
aanr <33546066+rnaufal32@users.noreply.github.com>
2023-06-23 08:54:27 +0700

    Create http.dart

---
+++ bricks/riverpod_init/__brick__/lib/src/service/http/http.dart
@@ -0,0 +1,60 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:logger/logger.dart';
+import 'package:{{app_name.snakeCase()}}/src/config/strings.dart';
+
+final httpServiceProvider = Provider.autoDispose((ref) {
+  final logger = Logger();
+
+  final dio = Dio(BaseOptions(baseUrl: AppStrings.baseUrl));
+
+  dio.options.contentType = Headers.jsonContentType;
+
+  dio.interceptors.add(InterceptorsWrapper(
+    onRequest: (request, handler) async {
+      // final token = await ref.read(firebaseAuthTokenProvider);
+      // final deviceInfo = ref.watch(deviceInfoServiceProvider);
+
+      // request.headers = {
+      //   ...request.headers,
+      //   'Accept': 'application/json',
+      //   'Authorization': 'Bearer $token',
+      //   'x-device-id': await deviceInfo.uid() ?? 'NULL',
+      //   'x-device-token': await ref.read(deviceTokenProvider),
+      // };
+
+      if (kDebugMode) {
+        logger.d(request.uri);
+        logger.d(request.headers);
+
+        if (request.data is FormData) {
+          final data = request.data as FormData;
+          for (final item in data.fields) {
+            logger.d('${item.key}: ${item.value}');
+          }
+          for (final item in data.files) {
+            logger.d('${item.key}: ${item.value}');
+          }
+        } else {
+          logger.d(request.data);
+        }
+      }
+
+      return handler.next(request);
+    },
+    onError: (e, handler) {
+      if (kDebugMode) {
+        logger.e(e.message, e.error, e.stackTrace);
+      }
+      return handler.next(e);
+    },
+    onResponse: (e, handler) {
+      if (kDebugMode) {
+        logger.d(e.data);
+      }
+      return handler.next(e);
+    },
+  ));
+  return dio;
+});
