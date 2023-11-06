fd5a1b6db461722c4c6fa5ff26e3cfe2c582b6cf
Abdul Aziz Naufal Ramadhan <33546066+rnaufal32@users.noreply.github.com>
2023-11-06 16:42:56 +0700

    Update post_gen.dart

--- bricks/riverpod_init/hooks/post_gen.dart
+++ bricks/riverpod_init/hooks/post_gen.dart
@@ -3,7 +3,7 @@ import 'package:mason/mason.dart';
 void run(HookContext context) {
   context.logger.info('Jangan lupa running cmd');
   context.logger.info(
-      'flutter pub add flutter_hooks hooks_riverpod flutter_screenutil dio retrofit freezed_annotation equatable auto_route hive hive_flutter jiffy logger');
+      'flutter pub add flutter_hooks hooks_riverpod scale_app gap dio retrofit freezed_annotation equatable auto_route hive hive_flutter jiffy logger');
   context.logger.info(
       'flutter pub add -d build_runner freezed auto_route_generator json_serializable retrofit_generator');
 }
