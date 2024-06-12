dee5d209465691ccd0c183783e52723ebb62619e
Abdul Aziz Naufal Ramadhan <33546066+rnaufal32@users.noreply.github.com>
2024-06-12 14:57:44 +0700

    Update post_gen.dart

--- bricks/riverpod_init/hooks/post_gen.dart
+++ bricks/riverpod_init/hooks/post_gen.dart
@@ -4,6 +4,6 @@ import 'package:mason/mason.dart';
 void run(HookContext context) {
   final progress = context.logger.progress('Installing packages');
   await Process.run('flutter', ['pub', 'add', 'flutter_hooks', 'hooks_riverpod', 'scaled_app', 'gap', 'dio', 'retrofit', 'freezed_annotation', 'equatable', 'auto_route', 'hive', 'hive_flutter', 'jiffy', 'logger', 'reactive_forms', 'firebase_core', 'firebase_auth']);
-  await Process.run('flutter', ['pub', 'add', -'d', 'build_runner', 'freezed', 'auto_route_generator', 'json_serializable', 'retrofit_generator']);
+  await Process.run('flutter', ['pub', 'add', '-d', 'build_runner', 'freezed', 'auto_route_generator', 'json_serializable', 'retrofit_generator']);
   progress.complete();
 }
