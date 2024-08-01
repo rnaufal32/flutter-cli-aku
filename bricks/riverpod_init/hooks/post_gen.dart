0d30a92affe9a08867d12a288e128716ec26a573
Abdul Aziz Naufal Ramadhan <33546066+rnaufal32@users.noreply.github.com>
2024-08-01 14:08:18 +0700

    Update post_gen.dart

--- bricks/riverpod_init/hooks/post_gen.dart
+++ bricks/riverpod_init/hooks/post_gen.dart
@@ -5,5 +5,6 @@ Future<void> run(HookContext context) async {
   final progress = context.logger.progress('Installing packages');
   await Process.run('flutter', ['pub', 'add', 'flutter_hooks', 'hooks_riverpod', 'scaled_app', 'gap', 'dio', 'retrofit', 'freezed_annotation', 'equatable', 'auto_route', 'hive', 'hive_flutter', 'jiffy', 'logger', 'reactive_forms', 'firebase_core', 'firebase_auth', 'google_fonts']);
   await Process.run('flutter', ['pub', 'add', '-d', 'build_runner', 'freezed', 'auto_route_generator', 'json_serializable', 'retrofit_generator']);
+  await Process.run('dart', ['run', 'build_runner', 'build', '-d']);
   progress.complete();
 }
