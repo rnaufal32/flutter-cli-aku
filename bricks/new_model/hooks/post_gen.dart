6abaf0abbf717a7f6ffd5dc9cc9954be1b7c0355
Abdul Aziz Naufal Ramadhan <33546066+rnaufal32@users.noreply.github.com>
2024-09-11 13:41:42 +0700

    Update post_gen.dart

--- bricks/new_model/hooks/post_gen.dart
+++ bricks/new_model/hooks/post_gen.dart
@@ -3,7 +3,7 @@ import 'package:mason/mason.dart';
 
 Future<void> run(HookContext context) async {
   final progress = context.logger.progress('Installing packages');
-  await Process.run('dart', ['pub', 'run', 'build_runner', 'build', '--delete-conflicting-outputs']);
+  // await Process.run('dart', ['pub', 'run', 'build_runner', 'build', '--delete-conflicting-outputs']);
   progress.complete();
 
 }
