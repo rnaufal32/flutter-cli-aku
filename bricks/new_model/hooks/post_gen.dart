99890ffe6a5dc535069fac76f07c1c3ec67fc225
Abdul Aziz Naufal Ramadhan <33546066+rnaufal32@users.noreply.github.com>
2024-09-13 08:22:39 +0700

    Delete bricks/new_model/hooks directory

--- bricks/new_model/hooks/post_gen.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-import 'package:mason/mason.dart';
-
-Future<void> run(HookContext context) async {
-  final progress = context.logger.progress('Installing packages');
-  // await Process.run('dart', ['pub', 'run', 'build_runner', 'build', '--delete-conflicting-outputs']);
-  progress.complete();
-
-}
